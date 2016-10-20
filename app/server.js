f4be66f63f7c3f4131ebdab1640bcf984e42bbb0
SilentRush <twilkis@hotmail.com>
2016-10-20 18:07:15 -0500

    test

--- app/server.js
+++ app/server.js
@@ -4,8 +4,9 @@ module.exports = {
   app: function () {
     const app = express()
     const indexPath = path.join(__dirname, 'index.html')
+    const publicPath = express.static('public')
 
-    app.use('/public', indexPath)
+    app.use(express.static('public'))
     app.get('/', function (_, res) { res.sendFile(indexPath) })
 
     return app
