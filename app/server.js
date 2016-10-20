86a18bf6d6471350184f77c1e5f9abc1d30190b0
SilentRush <twilkis@hotmail.com>
2016-10-20 17:34:23 -0500

    test

--- app/server.js
+++ app/server.js
@@ -1,6 +1,14 @@
 var express = require('express');
-var app = express();
+module.exports = {
+  app: function () {
+    const app = express()
+    const indexPath = path.join(__dirname, 'index.html')
+    const publicPath = express.static(path.join(__dirname, 'public'))
 
-app.use(express.static(__dirname + '/'));
+    app.use('/public', publicPath)
+    app.get('/', function (_, res) { res.sendFile(indexPath) })
+    app.listen(process.env.PORT || 8080);
 
-app.listen(process.env.PORT || 8080);
+    return app
+  }
+}
